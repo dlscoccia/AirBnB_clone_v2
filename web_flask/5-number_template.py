b951fa6d7930a294a8fc1d578e2253da2e38ea5b
dlscoccia <2054@holbertonschool.com>
2021-01-18 15:31:46 -0500

    Adding task 5-number_template & template 5.html; with pep8

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+'''
+task 5 module
+'''
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    '''
+    hello route
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    '''
+    hello HBNB
+    '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    '''
+    hello C
+    '''
+    text_fixed = "C " + text.replace("_", " ")
+    return text_fixed
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text="is cool"):
+    '''
+    hello Python <3
+    '''
+    text_fixed = "Python " + text.replace("_", " ")
+    return text_fixed
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hello_int(n):
+    '''
+    hello int number
+    '''
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def hello_int_template(n):
+    '''
+    hello int number in template
+    '''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
